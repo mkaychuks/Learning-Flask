00ccf37286ef29624bfd736ceec2db44e94bb33f
mkaychuks <sachufusi@gmail.com>
2020-07-08 02:21:51 +0100

    Added Forms with validators :smile:

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import(
+    Flask, render_template, url_for, flash, redirect
+)
 
 from forms import RegistrationForm, LoginForm
 
