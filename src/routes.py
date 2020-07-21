ad8b7da28f0cb3d947672e2469b9e3a409be35e9
mkaychuks <sachufusi@gmail.com>
2020-07-21 11:39:18 +0100

    Added pagination

--- src/routes.py
+++ src/routes.py
@@ -14,7 +14,8 @@ from src.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 
 @app.route('/')
 def home():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(per_page=5, page=page)
     return render_template('home.html', posts=posts)
 
 
